d89494822fa105e20a1cfeaf69e62b6cd6329390
Nana Adjei Manu <claeusdev@outlook.com>
2019-07-13 00:35:18 +0000

    add css-extract

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,4 +8,12 @@ module.exports = merge(commons, {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const path = require("path");
 const commons = require("./webpack.common");
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = merge(commons, {
   mode: "production",
@@ -9,5 +10,18 @@ module.exports = merge(commons, {
     filename: "[name].[contentHash].bundle.js",
     path: path.resolve(__dirname, "dist")
   },
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contentHash].[ext]"
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  }
 });
