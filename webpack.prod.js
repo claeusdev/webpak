ad3fdc41c91e2cd21b010b0dd7a5c1d3066ffb8a
Nana Adjei Manu <claeusdev@outlook.com>
2019-07-13 00:02:33 +0000

    add file-loader/clean-webpack-plugin/html-loader

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const commons = require("./webpack.common");
 const merge = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(commons, {
   mode: "production",
@@ -8,4 +9,5 @@ module.exports = merge(commons, {
     filename: "bundle.[contentHash].js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [new CleanWebpackPlugin()]
 });
